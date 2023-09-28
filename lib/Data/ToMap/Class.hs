44927b1832fc1a54a29053060bb8a55f243719af
Dan Dart <git@dandart.co.uk>
2023-09-28 18:30:21 +0100

    postpositive qualified

--- lib/Data/ToMap/Class.hs
+++ lib/Data/ToMap/Class.hs
@@ -1,7 +1,7 @@
 module Data.ToMap.Class where
 
-import           Data.Map
-import           Data.Text
+import Data.Map
+import Data.Text
 
 class ToMap a where
     toMap ∷ [a] → Map Text (Map Text Text)
